fc2cda49a4d8718c6f01d79420f3b0cb40968be1
zferdous35@gmail.com <zferdous35@gmail.com>
2020-11-30 03:48:25 +0600

    version 3.9

---
+++ app/src/main/java/com/example/noknok/HomeDishDisplay.java
@@ -0,0 +1,14 @@
+package com.example.noknok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeDishDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_dish_display);
+    }
+}
