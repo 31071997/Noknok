fe137086db3e169a1b902c38268a20146096ca96
Subah <subahcse11@gmail.com>
2021-06-24 21:03:00 +0600

    3.15

---
+++ app/src/main/java/com/example/noknok/Account_order.java
@@ -0,0 +1,14 @@
+package com.example.noknok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Account_order extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_order);
+    }
+}
