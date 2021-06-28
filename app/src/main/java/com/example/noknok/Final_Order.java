5d1b98ec0fb2e84ca460cff1ceb5c9ac481b96e3
Subah <subahcse11@gmail.com>
2021-06-29 00:43:00 +0600

    3.26

---
+++ app/src/main/java/com/example/noknok/Final_Order.java
@@ -0,0 +1,14 @@
+package com.example.noknok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Final_Order extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_order);
+    }
+}
