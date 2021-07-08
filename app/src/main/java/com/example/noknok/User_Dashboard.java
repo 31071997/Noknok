7589c3a530a90c5312ac5f1960456669c78a3279
Subah <subahcse11@gmail.com>
2021-07-08 14:53:09 +0600

    3.33

---
+++ app/src/main/java/com/example/noknok/User_Dashboard.java
@@ -0,0 +1,16 @@
+package com.example.noknok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import static com.example.noknok.R.layout.*;
+
+public class User_Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard);
+    }
+}
