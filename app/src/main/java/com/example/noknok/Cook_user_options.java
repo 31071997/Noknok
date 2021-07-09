4fc734e29fb15656644dbdc7c37c6a1113f9861c
Subah <subahcse11@gmail.com>
2021-07-10 00:27:01 +0600

    3.36

---
+++ app/src/main/java/com/example/noknok/Cook_user_options.java
@@ -0,0 +1,14 @@
+package com.example.noknok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cook_user_options extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cook_user_options);
+    }
+}
---
+++ app/src/main/java/com/example/noknok/Footer_Cook.java
@@ -0,0 +1,14 @@
+package com.example.noknok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Footer_Cook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_footer_cook);
+    }
+}
