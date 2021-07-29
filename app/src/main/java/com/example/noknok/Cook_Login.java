863c3f45644692ada3a613e2ae3fc5b4c25a2621
Subah <subahcse11@gmail.com>
2021-07-29 13:35:25 +0600

    3.46

---
+++ app/src/main/java/com/example/noknok/Cook_Login.java
@@ -0,0 +1,14 @@
+package com.example.noknok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cook_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cook_login);
+    }
+}
