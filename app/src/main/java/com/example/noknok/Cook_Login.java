2ad56542663f8686fe6902d26677f086db3348fd
31071997 <zferdous35@gmail.com>
2021-07-31 00:15:34 +0600

    3.48

--- app/src/main/java/com/example/noknok/Cook_Login.java
+++ app/src/main/java/com/example/noknok/Cook_Login.java
@@ -3,12 +3,14 @@ package com.example.noknok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Cook_Login extends AppCompatActivity {
-
+    TextView sign_up;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cook_login);
+        sign_up = (TextView)findViewById(R.id.sign_up_here);
     }
 }
--- app/src/main/java/com/example/noknok/MainActivity.java
+++ app/src/main/java/com/example/noknok/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.noknok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button cook_login,foodie_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        cook_login = (Button)findViewById(R.id.login_as_co);
+        foodie_login = (Button)findViewById(R.id.login_as_co);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cook_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Cook_Login.class);
+            }
+        });
+        foodie_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, login.class);
+            }
+        });
     }
 }
