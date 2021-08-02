ee2b1c3a29ae60cfc0c72a42e8296f049e17ac00
Subah <subahcse11@gmail.com>
2021-08-02 14:13:09 +0600

    3.51

---
+++ app/src/main/java/com/example/noknok/Order_and_payment_history.java
@@ -0,0 +1,14 @@
+package com.example.noknok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Order_and_payment_history extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_and_payment_history);
+    }
+}
